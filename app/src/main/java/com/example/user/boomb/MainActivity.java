cdb481394e240e1a4d20c6419a1b9efd9778f241
tayazhurba <taisiya_zhurba@mail.ru>
2015-06-28 04:08:26 +0800

    bombochka

--- app/src/main/java/com/example/user/boomb/MainActivity.java
+++ app/src/main/java/com/example/user/boomb/MainActivity.java
@@ -1,38 +1,44 @@
 package com.example.user.boomb;
 
-import android.support.v7.app.ActionBarActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.view.View;
+import android.widget.Button;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
-
+public class MainActivity extends Activity {
+    /** Called when the activity is first created. */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        final ImageView bomb = (ImageView) findViewById(R.id.bomb);
+        final ImageView fire = (ImageView) findViewById(R.id.fire);
+        final ImageView fume = (ImageView) findViewById(R.id.fume);
+        final Button btnStart = (Button) findViewById(R.id.btn_start);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Animation animbomb = AnimationUtils.loadAnimation(
+                        getApplicationContext(), R.anim.crazybomb);
+                bomb.startAnimation(animbomb);
+                Animation animfire = AnimationUtils.loadAnimation(
+                        getApplicationContext(), R.anim.crazyfire);
+                fire.startAnimation(animfire);
+                Animation animfume = AnimationUtils.loadAnimation(
+                        getApplicationContext(), R.anim.crazyfume);
+                fume.startAnimation(animfume);
+            }
+        });
     }
 }
